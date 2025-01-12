f236c2c58372fc1fa4dd3b6aa8bcdeede4f0f256
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2025-01-12 16:59:55 +0100

    Update decorator.py

--- unquad/utils/decorator.py
+++ unquad/utils/decorator.py
@@ -24,34 +24,28 @@ def performance_conversion(*arg_names):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            # Helper function to convert lists to numpy arrays, including nested lists
             def convert_to_array(value):
                 if isinstance(value, list):
                     try:
                         return np.array(value)
                     except ValueError:
-                        # Handle irregular nested lists if necessary
                         return [
                             np.array(v) if isinstance(v, list) else v for v in value
                         ]
                 return value
 
-            # Convert kwargs
             new_kwargs = {
                 k: convert_to_array(v) if k in arg_names else v
                 for k, v in kwargs.items()
             }
 
-            # Convert args
             args = tuple(
                 convert_to_array(arg) if arg_name in arg_names else arg
                 for arg, arg_name in zip(args, func.__code__.co_varnames)
             )
 
-            # Execute the function with modified arguments
             result = func(*args, **new_kwargs)
 
-            # Convert numpy arrays back to lists in the result
             def convert_result(r):
                 if isinstance(r, np.ndarray):
                     return r.tolist()
