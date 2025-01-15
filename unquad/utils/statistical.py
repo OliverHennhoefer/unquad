27bae8a37d041ec9ccc9763a279961b968a485b7
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2025-01-15 20:32:48 +0100

    Updates requirements.txt, Code Clean-Up, Naming

--- unquad/utils/statistical.py
+++ unquad/utils/statistical.py
@@ -23,8 +23,8 @@ def calculate_p_val(scores: np.array, calibration_set: np.array) -> np.array:
         The p-value for each score is calculated using the formula:
             p_val = (1 + number of calibration_set values >= score) / (1 + number of calibration_set values)
     """
-    p_val = np.sum(calibration_set >= scores[:, np.newaxis], axis=1)
-    return (1.0 + p_val) / (1.0 + len(calibration_set))
+    sum_smaller = np.sum(calibration_set >= scores[:, np.newaxis], axis=1)
+    return (1.0 + sum_smaller) / (1.0 + len(calibration_set))
 
 
 @performance_conversion("scores")
