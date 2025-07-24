7bde3e8d61d39e82c326c562d45b8a293ddb333e
Oliver Hennhöfer <oliver.hennhoefer@mail.de>
2025-07-24 14:54:55 +0200

    Incremets version

--- nonconform/__init__.py
+++ nonconform/__init__.py
@@ -10,6 +10,6 @@ Main Components:
 - Statistical utilities and data handling tools
 """
 
-__version__ = "0.9.11"
+__version__ = "0.9.12"
 __author__ = "Oliver Hennhoefer"
 __email__ = "oliver.hennhoefer@mail.de"
